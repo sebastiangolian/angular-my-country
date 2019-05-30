b1c79c9a995766197ace09ca72b565ae587a786a
sebastiangolian <sebastiangolian@gmail.com>
2019-05-30 12:43:13 +0200

    rename moje-panstwo component dir name

--- src/app/moje-panstwo/moje-panstwo.module.ts
+++ src/app/moje-panstwo/moje-panstwo.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MojePanstwoPaginationComponent } from './components/pagination/moje-panstwo-pagination.component';
+import { MojePanstwoPaginationComponent } from './components/moje-panstwo-pagination/moje-panstwo-pagination.component';
 import { DataSetToLinksPipe } from './pipes/dataset-to-links.pipe';
 import { DataSetToDataPipe } from './pipes/dataset-to-data.pipe';
 import { DataSetToDataArrayPipe } from './pipes/dataset-to-data-array.pipe';
